c4f3717d727bbb26a86c73f4945908401c06ef8b
adv8j <advait.jain2003@gmail.com>
2023-12-08 21:40:07 +0530

    added timer

--- quordle/src/app/page.js
+++ quordle/src/app/page.js
@@ -24,7 +24,11 @@ const Home = () => {
   };
 
   const getLetterColor = (rowIndex, cell, colIndex) => {
-    if (!isRowComplete(rowIndex)) {
+    if (
+      !isRowComplete(rowIndex) ||
+      !inputRefs.current[rowIndex][gridSize - 1].current.querySelector("input")
+        .disabled
+    ) {
       return "transparent"; // Keep transparent until the row is complete
     }
 
@@ -39,9 +43,44 @@ const Home = () => {
     return "transparent"; // Default background
   };
 
+  // implementing timer
+  const [timer, setTimer] = useState(0);
+  const timerRequestRef = useRef(null);
+  const startTime = useRef(null);
+
+  const startTimer = () => {
+    startTime.current = performance.now();
+    const updateTimer = (timestamp) => {
+      const elapsed = timestamp - startTime.current;
+      setTimer(elapsed);
+      timerRequestRef.current = requestAnimationFrame(updateTimer);
+    };
+
+    timerRequestRef.current = requestAnimationFrame(updateTimer);
+  };
+
+  const stopTimer = () => {
+    cancelAnimationFrame(timerRequestRef.current);
+  };
+
+  const resetTimer = () => {
+    cancelAnimationFrame(timerRequestRef.current);
+    setTimer(0);
+  };
+
+  const formatTime = (milliseconds) => {
+    const seconds = Math.floor(milliseconds / 1000);
+    const remainingMilliseconds = Math.floor(milliseconds % 1000);
+    return `${seconds}:${remainingMilliseconds < 100 ? '0' : ''}${remainingMilliseconds}`;
+  };
+
+  // console.log("Timer:", timer);
   const handleInputChange = (row, col, value) => {
     // Update the guesses state with the new value, ensuring only the first character is used
     if (/^[a-zA-Z]$/.test(value.key)) {
+      if (row === 0 && col === 0 && timer === 0) {
+        startTimer();
+      }
       value.target.value = value.key.toUpperCase();
       const newGuesses = guesses.map((currentRow, rowIndex) =>
         rowIndex === row
@@ -76,8 +115,7 @@ const Home = () => {
             return;
           }
         }
-  
-        // Check if word is complete and valid
+
         if (!dictionary.includes(word.toUpperCase())) {
           // If word is not in the dictionary
           toast.error("Not in Word List!");
@@ -85,22 +123,31 @@ const Home = () => {
           // When word is valid and new
           if (word === WORD) {
             toast.success("You Guessed It!");
+            stopTimer();
             // Disable further input after guessing correctly
             for (let x = row; x < guessesAllowed; x++) {
               for (let y = 0; y < gridSize; y++) {
-                inputRefs.current[x][y].current.querySelector("input").disabled = true;
+                inputRefs.current[x][y].current.querySelector(
+                  "input"
+                ).disabled = true;
               }
             }
           } else {
             // Handle end of game or moving to the next row
             if (row === guessesAllowed - 1) {
               toast.error("Game Over!");
+              stopTimer();
             } else {
-              inputRefs.current[row + 1][0].current.querySelector("input").focus();
+              inputRefs.current[row + 1][0].current
+                .querySelector("input")
+                .focus();
             }
           }
           // Disable the current row
-          inputRefs.current[row].forEach((ref) => ref.current.querySelector("input").disabled = true);
+          inputRefs.current[row].forEach(
+            (ref) => (ref.current.querySelector("input").disabled = true)
+          );
+          setGuesses([...guesses]); // Update the state to trigger re-render
         }
       } else {
         toast.error("Word Not Complete!");
@@ -111,7 +158,9 @@ const Home = () => {
       let isFocused = false;
       for (let x = 0; x < guessesAllowed; x++) {
         for (let y = 0; y < gridSize; y++) {
-          if (!inputRefs.current[x][y].current.querySelector("input").disabled) {
+          if (
+            !inputRefs.current[x][y].current.querySelector("input").disabled
+          ) {
             inputRefs.current[x][y].current.querySelector("input").focus();
             isFocused = true;
             break;
@@ -120,7 +169,7 @@ const Home = () => {
         if (isFocused) break;
       }
     }
-  
+
     // Move focus to the next or previous cell
     if (value.target.value.length === 1 && col < gridSize - 1) {
       inputRefs.current[row][col + 1].current.querySelector("input").focus();
@@ -128,7 +177,6 @@ const Home = () => {
       inputRefs.current[row][col - 1].current.querySelector("input").focus();
     }
   };
-  
 
   useEffect(() => {
     // Focus the first input on initial render
@@ -193,7 +241,8 @@ const Home = () => {
                   margin: "0 4px",
                   "& .MuiOutlinedInput-root": {
                     "& fieldset": {
-                      borderColor: letterColor !== "transparent" ? letterColor : "white",
+                      borderColor:
+                        letterColor !== "transparent" ? letterColor : "white",
                     },
                     "&:hover fieldset": {
                       borderColor: "white",
@@ -211,6 +260,7 @@ const Home = () => {
         </Box>
       ))}
       <Toaster />
+      <Typography variant="h4">Timer: {formatTime(timer)} </Typography>
     </Container>
   );
 };
