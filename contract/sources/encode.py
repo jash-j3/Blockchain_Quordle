921c03d7351c91045a282949acd8b2d83e9fea03
kst164 <kartheek@tammana.org>
2023-12-10 22:57:53 +0530

    smaller wordlist because of max package size limit

--- contract/sources/encode.py
+++ contract/sources/encode.py
@@ -6,6 +6,7 @@ def encode(word):
     return encoded
 
 
+# wordlist from https://raw.githubusercontent.com/charlesreid1/five-letter-words/master/sgb-words.txt
 with open('words.txt', 'r') as f:
     for line in f:
         word = line.strip()
